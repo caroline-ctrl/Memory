c81e6fd64e13a01fca3e470099290edd229ce988
caroline-ctrl <duon.caroline@gmail.com>
2020-04-09 19:53:03 +0200

    régles misent en place

--- script.js
+++ script.js
@@ -41,6 +41,164 @@ for (let i = 0; i < result2.length; i++){
 }
 
 
+let tableau = []
+//////////////////////////////
+// découvrement des cartes
+function decouvreUser(value)
+{
+    if (value == 0){
+        unie0.style.display = "none"
+        tableau.push(document.getElementById("nbr0").src)
+        tableau.push(unie0)
+    } else if (value == 1){
+        unie1.style.display = "none"
+        tableau.push(document.getElementById("nbr1").src)
+        tableau.push(unie1)
+    } else if (value == 2){
+        unie2.style.display = "none"
+        tableau.push(document.getElementById("nbr2").src)
+        tableau.push(unie2)
+    } else if (value == 3){
+        unie3.style.display = "none"
+        tableau.push(document.getElementById("nbr3").src)
+        tableau.push(unie3)
+    } else if (value == 4){
+        unie4.style.display = "none"
+        tableau.push(document.getElementById("nbr4").src)
+        tableau.push(unie4)
+    } else if (value == 5){
+        unie5.style.display = "none"
+        tableau.push(document.getElementById("nbr5").src)
+        tableau.push(unie5)
+    } else if (value == 6){
+        unie6.style.display = "none"
+        tableau.push(document.getElementById("nbr00").src)
+        tableau.push(unie6)
+    } else if (value == 7){
+        unie7.style.display = "none"
+        tableau.push(document.getElementById("nbr11").src)
+        tableau.push(unie7)
+    } else if (value == 8){
+        unie8.style.display = "none"
+        tableau.push(document.getElementById("nbr22").src)
+        tableau.push(unie8)
+    } else if (value == 9){
+        unie9.style.display = "none"
+        tableau.push(document.getElementById("nbr33").src)
+        tableau.push(unie9)
+    } else if (value == 10){
+        unie10.style.display = "none"
+        tableau.push(document.getElementById("nbr44").src)
+        tableau.push(unie10)
+    } else if (value == 11){
+        unie11.style.display = "none"
+        tableau.push(document.getElementById("nbr55").src)
+        tableau.push(unie11)
+    }
 
-/////////////////////
-// couvrir les cartes
+console.log(tableau)
+
+timetime()
+}
+
+
+function timetime()
+{
+    setTimeout(verif, 2000)
+}
+
+
+
+
+//////////////////////////////////////////////
+// comparaison avec les deux cartes retournées
+function verif()
+{
+
+
+    if (tableau[0] != tableau[2]){
+        tableau[1].style.display = "block"
+        tableau[3].style.display = "block"
+        tableau.splice(0, 5)
+    } else {
+        tableau[1].style.display = "none"
+        tableau[3].style.display = "none"
+        tableau.splice(0, 5)
+
+    }
+
+
+    // let nbr1 = document.getElementById('nbr0').src
+    // let nbr2 = document.getElementById('nbr1').src
+    // let nbr3 = document.getElementById('nbr2').src
+    // let nbr4 = document.getElementById('nbr3').src
+    // let nbr5 = document.getElementById('nbr4').src
+    // let nbr6 = document.getElementById('nbr5').src
+    // let nbr11 = document.getElementById('nbr00').src
+    // let nbr22 = document.getElementById('nbr11').src
+    // let nbr33 = document.getElementById('nbr22').src
+    // let nbr44 = document.getElementById('nbr33').src
+    // let nbr55 = document.getElementById('nbr44').src
+
+
+
+    
+    // if (nbr0 == nbr00){
+    //     unie0.style.display = "none"
+    //     unie6.style.display = "none"
+    // } else {
+    //     unie0.style.display = "block"
+    //     unie6.style.display = "block"
+    // }
+
+    // if (nbr1 == nbr11){
+    //     unie1.style.display = "none"
+    //     unie7.style.display = "none"
+    // } else {
+    //     unie1.style.display = "block"
+    //     unie7.style.display = "block"
+    // }
+
+    // if (nbr2 == nbr22){
+    //     unie2.style.display = "none"
+    //     unie8.style.display = "none"
+    // } else {
+    //     unie2.style.display = "block"
+    //     unie8.style.display = "block"
+    // }
+
+    // if (nbr3 == nbr33){
+    //     unie3.style.display = "none"
+    //     unie9.style.display = "none"
+    // } else {
+    //     unie3.style.display = "block"
+    //     unie9.style.display = "block"
+    // }
+
+    // if (nbr4 == nbr44){
+    //     unie4.style.display = "none"
+    //     unie10.style.display = "none"
+    // } else {
+    //     unie4.style.display = "block"
+    //     unie10.style.display = "block"
+    // }
+
+    // if (nbr5 == nbr55){
+    //     unie5.style.display = "none"
+    //     unie11.style.display = "none"
+    // } else {
+    //     unie5.style.display = "block"
+    //     unie11.style.display = "block"
+    // }
+
+
+
+
+
+
+    // if ((recup == 0) && (recup == )){
+    //     unie0.style.display = "block"
+    // }
+    // si nom de l'image != du nom de l'image
+    // .style.display = "block"
+}
